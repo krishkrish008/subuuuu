c8dd9f108f3b6998deebae9fd827a1a30bc4a512
krishkrish008 <35235157+krishkrish008@users.noreply.github.com>
2018-01-22 16:52:10 +0530

    subusiva.c

---
+++ subusiva.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("Enter value of a");
+scanf("%d",&a);
+if( (a%a) && (a%1) )
+{
+printf("It is a prime number");
+}
+else
+{
+printf("Not a prime number");
+}
+return 0;
+}
